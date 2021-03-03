346dc52e9777a8504ea9d12ea7c693eb1dea254d
nytrist <jacinta@nytrist.com>
2021-03-03 22:39:55 +1100

    reverting back

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import os
-
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -14,10 +12,13 @@ from resources.gateway import Gateway, GatewayMod, GatewayList
 
 app  = Flask(__name__)
 
-app.config['DEBUG'] = True
+#create db if not created
+@app.before_first_request
+def create_tables():
+	db.create_all()
 
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "jc"
 api = Api(app)
@@ -48,10 +49,4 @@ api.add_resource(UserRegister, '/register')
 if __name__ == '__main__':
 	from db import db
 	db.init_app(app)
-
-	if app.config['DEBUG']:
-		@app.before_first_request
-		def create_tables():
-			db.create_all()
-
 	app.run(port=5000, debug=True)
